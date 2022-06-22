47c9d4fab156ce718f67665415bc60f9e039cf23
mart1d4 <mart1danton@gmail.com>
2022-06-22 23:01:31 +0200

    Boom

---
+++ index.js
@@ -0,0 +1,8 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Intents } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.commands = new Collection();
